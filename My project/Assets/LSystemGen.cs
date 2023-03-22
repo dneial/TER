432fe7d44497063b6a81a5088ce0258782b72d8f
Sebastien <seb97x@hotmail.fr>
2023-03-22 11:08:24 +0100

    cleaning du dossier

--- My project/Assets/LSystemGen.cs
+++ My project/Assets/LSystemGen.cs
@@ -3,7 +3,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ScriptGen1 : MonoBehaviour
+public class LSystemGen : MonoBehaviour
 {
     Lsystem lsystem;
 
