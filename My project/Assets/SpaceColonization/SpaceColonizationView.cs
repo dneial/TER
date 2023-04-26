e71b37a0fd169352e8eaa725ac8d5e6d0f1a1bf7
Tom Simula <tom.simul@gmail.com>
2023-04-26 10:25:12 +0200

    Thickness

--- My project/Assets/SpaceColonization/SpaceColonizationView.cs
+++ My project/Assets/SpaceColonization/SpaceColonizationView.cs
@@ -46,14 +46,33 @@ public class SpaceColonizationView {
                 capsule.name = "Node(" + node.parent.id + ", " + node.id + ")";
                 capsule.transform.position = (node.position + node.parent.position) / 2f;
                 Debug.Log(node.thickness);
-                var actualThickness = thickness.Evaluate(node.thickness);
+                float actualThickness = thickness.Evaluate(node.thickness);
                 capsule.transform.localScale = new Vector3(actualThickness, (actualThickness+Vector3.Distance(node.position, node.parent.position))/2, actualThickness);
+                if(node.parent != null){
+                    MajVerticesCapsule(capsule, actualThickness,thickness.Evaluate(node.parent.thickness));
+                } else {
+                    MajVerticesCapsule(capsule, actualThickness,1);
+                }
                 capsule.transform.parent = this.root.transform;
                 capsule.transform.LookAt(node.position);
                 capsule.transform.Rotate(Vector3.right, 90);
                 node.cylindre = capsule;
             }
         }
+    }
+
+    private void MajVerticesCapsule(GameObject caps, float topThickness, float botThickness){
+        Mesh myMesh = caps.transform.GetComponent<MeshFilter>().mesh;
+        var myVertices = new Vector3[myMesh.vertices.Length];
+        for (int i = 0; i < myMesh.vertices.Length; i++) {
+            if(myMesh.vertices[i].y > 0){
+                myVertices[i] = new Vector3(myMesh.vertices[i].x*topThickness, myMesh.vertices[i].y, myMesh.vertices[i].z*topThickness);
+            } else {
+                myVertices[i] = new Vector3(myMesh.vertices[i].x*botThickness, myMesh.vertices[i].y, myMesh.vertices[i].z*botThickness);
+            }
+        }
+        myMesh.SetVertices(myVertices);
+        caps.transform.GetComponent<MeshFilter>().mesh = myMesh;
     }
 
     public void DropLeaves(List<Leaf> leaves)
